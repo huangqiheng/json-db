13d80e40345ea3354bac08314c73918193a173ef
huangqiheng <huangqiheng@gmail.com>
2015-04-21 09:27:50 +0800

    modify config.php

--- admin/config.php
+++ admin/config.php
@@ -3,3 +3,6 @@
 //客户端使用什么模式来访问数据，有时候服务器会不允许直接访问json格式文件
 define('PORT_MODE', true);
 
+//禁止注册
+define('DISABLE_CREATE', true);
+
--- admin/functions.php
+++ admin/functions.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'login.php';
 require_once 'config.php';
+require_once 'login.php';
 
 /*****************************************
 	系统常量
